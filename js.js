70eadc84f42b11990e4048ec187cb330cee2cce8
hieu011 <129260843+hieu011@users.noreply.github.com>
2023-05-03 10:41:40 +0700

    case study

---
+++ js.js
@@ -0,0 +1,157 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Fried shrimp with cheese',
+        image: 'product_1.png',
+        price: 120000
+    },
+    {
+        id: 2,
+        name: 'Avocado and egg toast',
+        image: 'product_2.png',
+        price: 120000
+    },
+    {
+        id: 3,
+        name: 'Green house salad',
+        image: 'product_3.png',
+        price: 220000
+    },
+    {
+        id: 4,
+        name: 'Chicken burger',
+        image: 'product_4.png',
+        price: 123000
+    },
+    {
+        id: 5,
+        name: 'Classic ceasar salad',
+        image: 'product_5.png',
+        price: 320000
+    },
+    {
+        id: 6,
+        name: 'Grilled fish with orange sauce',
+        image: 'product_6.png',
+        price: 120000
+    },
+    {
+        id: 7,
+        name: 'Fred beef fillet five color',
+        image: 'product_7.png',
+        price: 120000
+    },
+    {
+        id: 8,
+        name: 'Roadted chorizo with cheese sauce',
+        image: 'product_8.png',
+        price: 120000
+    },
+    {
+        id: 9,
+        name: 'Pasta Bolognese',
+        image: 'product_9.png',
+        price: 120000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="assets/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="assets/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
+
+
+// next-prev comment
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const comment = document.querySelector('#list-comment')
+const commentItem = document.querySelectorAll('#list-comment .item')
+var translateY = 0
+var count = commentItem.length
+
+next.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 1) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += -400
+  comment.style.transform = `translateY(${translateY}px)`
+  count--
+})
+
+prev.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 3) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += 400
+  comment.style.transform = `translateY(${translateY}px)`
+  count++
+})
